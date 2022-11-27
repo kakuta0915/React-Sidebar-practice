2319ae2e0f95b6c0da3b8ac9656db9515adeea9e
takuru kakuta <kakuta19960915@gmail.com>
2022-11-27 20:02:37 +0900

    sidebar-commit

---
+++ src/components/SidebarIcon.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Icon from '../images/icon.png'
+function SidebarIcon() {
+    return (
+        <div className="SidebarIcon">
+            <img src={Icon} alt="" />
+            <p>programing@gmail.com</p>
+        </div>
+    )
+}
+
+export default SidebarIcon
